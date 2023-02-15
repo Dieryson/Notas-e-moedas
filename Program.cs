71531981921fc53bc63e9b121c2063ee485eb6dc
Dieryson Araujo <115048301+Dieryson@users.noreply.github.com>
2023-02-15 15:40:35 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System; 
+
+class Program {
+
+    static void Main(string[] args) { 
+
+        double dinheiro = Convert.ToDouble(Console.ReadLine());
+        double[] valor = { 100, 50, 20, 10, 5, 2, 1, 0.5, 0.25, 0.1, 0.05, 0.01 };
+        int quantidadedenotas;
+        
+        Console.WriteLine("NOTAS:");
+
+        for (int i=0;i<6;i++)
+        {
+            quantidadedenotas = (int)(dinheiro / valor[i]);
+            Console.WriteLine(quantidadedenotas + " nota(s) de R$ " + valor[i].ToString("0.00"));
+            dinheiro -= quantidadedenotas * valor[i];
+            dinheiro = Math.Round(dinheiro, 2);
+        }
+        
+        Console.WriteLine("MOEDAS:");
+        for (int i = 6; i < valor.Length; i++)
+        {
+            quantidadedenotas = (int)(dinheiro / valor[i]);
+            Console.WriteLine(quantidadedenotas + " moeda(s) de R$ " + valor[i].ToString("0.00"));
+            dinheiro -= quantidadedenotas * valor[i];
+            dinheiro = Math.Round(dinheiro, 2);
+        }
+        Console.ReadKey();
+
+    }
+}
